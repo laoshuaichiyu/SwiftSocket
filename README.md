500770b086accbc049e63930291e76f1b2dad71f
laoshuaichiyu <zzhhuise@gmail.com>
2019-03-07 17:02:20 +0800

    delete projcet

--- README.md
+++
@@ -1,3 +0,0 @@
-# SwiftSocket
-
-A description of this package.
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import SwiftSocketTests
-
-var tests = [XCTestCaseEntry]()
-tests += SwiftSocketTests.allTests()
-XCTMain(tests)
